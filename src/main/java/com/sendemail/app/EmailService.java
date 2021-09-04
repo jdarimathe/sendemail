5e3f9e83e87edb66c19ca00ba304f8153e25adc8
jdarimathe <joseph.darimathe@gmail.com>
2021-09-03 22:36:47 -0400

    Send email with my spring boot app

---
+++ src/main/java/com/sendemail/app/EmailService.java
@@ -0,0 +1,57 @@
+package com.sendemail.app;
+
+import java.io.File;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	JavaMailSender javaMailSender;
+
+	public String sendEmail() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setFrom("joseph.darimathe@gmail.com");
+		message.setTo("mathieuplouis@gmail.com");
+		message.setCc("juleswilfrid9@gmail.com");
+		message.setSubject("Test Subject My Spring Boot App");
+		message.setText("Test Body My Spring Boot App, Just trying to send a mail from my spring boot app");
+		
+		javaMailSender.send(message);
+		
+		return "Mail sent successfully";
+	}
+	
+//	public String sendEmailwithAttachment() {
+//		try {
+//			MimeMessage message = javaMailSender.createMimeMessage();
+//
+//			MimeMessageHelper messageHelper =
+//					new MimeMessageHelper(message, true);
+//
+//			messageHelper.setFrom("");
+//			messageHelper.setTo("");
+//			messageHelper.setSubject("Test Subject");
+//			messageHelper.setText("Test Body");
+//
+//			File file = new File("Path To File");
+//
+//			messageHelper.addAttachment(file.getName(), file);
+//
+//			javaMailSender.send(message);
+//
+//			return "Mail sent successfully";
+//
+//		} catch (Exception e) {
+//			return "Mail sent failed";
+//		}
+//	}
+}
